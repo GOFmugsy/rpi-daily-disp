decff97d8cb0510d04880f9ccfbaa9ac048eb64b
Jeff Monroe <jeff.monroe@gmail.com>
2021-11-13 09:13:24 -0500

    working top 3 articles

--- get-headlines.py
+++ get-headlines.py
@@ -2,11 +2,16 @@ import urllib.request
 import json
 
 api = ""
-
 apiJson = urllib.request.urlopen("https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=" + api).read()
-
 decode = apiJson.decode('utf-8')
-
 apiDict = json.loads(decode)
 
-print(apiDict.keys())
+i = 0
+for article in apiDict["results"]:
+	i = i + 1
+	if i > 3:
+		continue
+	print(article["title"])
+	print(article["abstract"])
+	print()
+
