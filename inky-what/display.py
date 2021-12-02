6a990d0cbbfbda02e3104f804e027fa7f60714a8
GOFmugsy <jeff.monroe@gmail.com>
2021-12-02 02:33:12 +0000

    getting a start on displaying stuff

---
+++ inky-what/display.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from PIL import Image, ImageFont, ImageDraw
+from font_hanken_grotesk import HankenGroteskBold, HankenGroteskMedium
+from font_intuitive import Intuitive
+from inky.auto import auto
+
+try:
+    inky_display = auto(ask_user=True, verbose=True)
+except TypeError:
+    raise TypeError("You need to update the Inky library to >= v1.1.0")
+
+parser = argparse.ArgumentParser()
+# parser.add_argument('--name', '-n', type=str, required=True, help="Your name")
+args, _ = parser.parse_known_args()
+
+# inky_display.set_rotation(180)
+try:
+    inky_display.set_border(inky_display.RED)
+except NotImplementedError:
+    pass
+
+# Figure out scaling for display size
+
+scale_size = 1.0
+padding = 0
+
+if inky_display.resolution == (400, 300):
+    scale_size = 2.20
+    padding = 15
+
+if inky_display.resolution == (600, 448):
+    scale_size = 2.20
+    padding = 30
+
+if inky_display.resolution == (250, 122):
+    scale_size = 1.30
+    padding = -5
+
+# Create a new canvas to draw on
+
+img = Image.new("P", inky_display.resolution)
+draw = ImageDraw.Draw(img)
+
+# Load the fonts
+
+intuitive_font = ImageFont.truetype(Intuitive, int(22 * scale_size))
+hanken_bold_font = ImageFont.truetype(HankenGroteskBold, int(35 * scale_size))
+hanken_medium_font = ImageFont.truetype(HankenGroteskMedium, int(16 * scale_size))
+
+# Grab the name to be displayed
+
+# name = args.name
+
+# borders
+
+# weather-box
+
+weatherTop = 0
+weatherBottom = int(inky_display.height * .4)
+weatherLeft = int(inky_display.width - (inky_display.width * .3))
+weatherRight = int(inky_display.width)
+
+for y in range(weatherTop, weatherBottom):
+    img.putpixel((weatherLeft,y), inky_display.BLACK)
+for x in range(weatherLeft, weatherRight):
+    img.putpixel((x,weatherBottom), inky_display.BLACK)
+
+
+# headlines-box
+
+headTop = weatherBottom
+headBottom = inky_display.height
+headLeft = 0
+headRight = inky_display.width
+
+for x in range(headLeft, headRight):
+    img.putpixel((x,headTop), inky_display.BLACK)
+
+# cal-box
+
+# Draw the red, white, and red strips
+
+# for y in range(0, y_top):
+    # for x in range(0, inky_display.width):
+        # img.putpixel((x, y), inky_display.BLACK if inky_display.colour == "black" else inky_display.RED)
+
+# for y in range(y_top, y_bottom):
+    # for x in range(0, inky_display.width):
+        # img.putpixel((x, y), inky_display.WHITE)
+
+# for y in range(y_bottom, inky_display.height):
+    # for x in range(0, inky_display.width):
+        # img.putpixel((x, y), inky_display.BLACK if inky_display.colour == "black" else inky_display.RED)
+
+# Calculate the positioning and draw the "Hello" text
+
+# hello_w, hello_h = hanken_bold_font.getsize("Hello")
+# hello_x = int((inky_display.width - hello_w) / 2)
+# hello_y = 0 + padding
+# draw.text((hello_x, hello_y), "Hello", inky_display.WHITE, font=hanken_bold_font)
+
+# Calculate the positioning and draw the "my name is" text
+
+# mynameis_w, mynameis_h = hanken_medium_font.getsize("my name is")
+# mynameis_x = int((inky_display.width - mynameis_w) / 2)
+# mynameis_y = hello_h + padding
+# draw.text((mynameis_x, mynameis_y), "my name is", inky_display.WHITE, font=hanken_medium_font)
+
+# Calculate the positioning and draw the name text
+
+# name_w, name_h = intuitive_font.getsize(name)
+# name_x = int((inky_display.width - name_w) / 2)
+# name_y = int(y_top + ((y_bottom - y_top - name_h) / 2))
+# draw.text((name_x, name_y), name, inky_display.BLACK, font=intuitive_font)
+
+# Display the completed name badge
+
+inky_display.set_image(img)
+inky_display.show()
